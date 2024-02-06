808dc4785a9f3b7327b01881ac74ac8e8f07bcaf
Aushzlten12 <josepachas1604@gmail.com>
2024-02-05 23:20:33 -0500

    Refactoring

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Note from './components/Note'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
 import noteService from './services/notes'
+import loginService from './services/login'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [user, setUser] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
@@ -31,12 +33,91 @@ const App = () => {
     })
   }
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
-    console.log('loggin in with ', username, password)
-    setUsername('')
-    setPassword('')
+    try {
+      const userCredentials = await loginService.login({
+        username,
+        password,
+      })
+      setUser(userCredentials)
+      console.log(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
+
+  // Functions for forms
+  const loginForm = () => (
+    <form
+      onSubmit={handleLogin}
+      className="max-w-xs mx-auto mt-8 bg-gray-100 p-8 rounded shadow-md"
+    >
+      <div className="mb-4">
+        <label
+          htmlFor="username"
+          className="block text-gray-700 font-bold mb-2"
+        >
+          Username
+        </label>
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          className="w-full py-2 px-3 border border-gray-300 rounded focus:outline-none focus:border-indigo-500 placeholder-gray-500 placeholder-opacity-50"
+          onChange={({ target }) => setUsername(target.value)}
+          placeholder="Enter your username"
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          htmlFor="password"
+          className="block text-gray-700 font-bold mb-2"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          className="w-full py-2 px-3 border border-gray-300 rounded focus:outline-none focus:border-indigo-500 placeholder-gray-500 placeholder-opacity-50"
+          onChange={({ target }) => setPassword(target.value)}
+          placeholder="Enter your password"
+        />
+      </div>
+      <button
+        type="submit"
+        className="bg-indigo-500 text-white py-2 px-4 rounded hover:bg-indigo-700 focus:outline-none active:bg-indigo-800"
+      >
+        Login
+      </button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form
+      className="max-w-xl mx-auto mt-4 bg-white p-4 rounded"
+      onSubmit={addNote}
+    >
+      <input
+        className="w-full py-2 px-3 border border-gray-300 rounded focus:outline-none focus:border-indigo-300"
+        value={newNote}
+        onChange={handleNoteChange}
+        placeholder="Enter a new note"
+      />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3"
+        type="submit"
+      >
+        Save
+      </button>
+    </form>
+  )
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
@@ -70,49 +151,17 @@ const App = () => {
         <h1 className="text-5xl to-zinc-900">Notes</h1>
       </div>
       <Notification message={errorMessage} />
-      <form
-        onSubmit={handleLogin}
-        className="max-w-xs mx-auto mt-8 bg-gray-100 p-8 rounded shadow-md"
-      >
-        <div className="mb-4">
-          <label
-            htmlFor="username"
-            className="block text-gray-700 font-bold mb-2"
-          >
-            Username
-          </label>
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            className="w-full py-2 px-3 border border-gray-300 rounded focus:outline-none focus:border-indigo-500 placeholder-gray-500 placeholder-opacity-50"
-            onChange={({ target }) => setUsername(target.value)}
-            placeholder="Enter your username"
-          />
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          <p className="text-2xl">
+            {' '}
+            <span className=" font-bold px-4">{user.name}</span> logged-in
+          </p>
+          {noteForm()}
         </div>
-        <div className="mb-4">
-          <label
-            htmlFor="password"
-            className="block text-gray-700 font-bold mb-2"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            className="w-full py-2 px-3 border border-gray-300 rounded focus:outline-none focus:border-indigo-500 placeholder-gray-500 placeholder-opacity-50"
-            onChange={({ target }) => setPassword(target.value)}
-            placeholder="Enter your password"
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-indigo-500 text-white py-2 px-4 rounded hover:bg-indigo-700 focus:outline-none active:bg-indigo-800"
-        >
-          Login
-        </button>
-      </form>
+      )}
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-20"
         onClick={() => setShowAll(!showAll)}
@@ -128,19 +177,6 @@ const App = () => {
           />
         ))}
       </ul>
-      <form className="ml-20 w-fit flex space-x-4" onSubmit={addNote}>
-        <input
-          className="border rounded py-2 px4"
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          type="submit"
-        >
-          save
-        </button>
-      </form>
       <Footer />
     </>
   )
